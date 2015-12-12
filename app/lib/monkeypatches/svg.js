05314431c524102e18f386737b4bcd44027633bb
Laszlo Korte <me@laszlokorte.de>
2015-12-12 05:21:54 +0100

    code style

--- app/lib/monkeypatches/svg.js
+++ app/lib/monkeypatches/svg.js
@@ -3,7 +3,9 @@
 
 var isArray = require('x-is-array');
 
-import {h} from '@cycle/dom';
+// START Cycle.js-specific code >>>>>>>>
+var h = require('@cycle/dom').h;
+// END Cycle.js-specific code <<<<<<<<<<
 
 var SVGAttributeNamespace = require('virtual-dom/virtual-hyperscript/svg-attribute-namespace');
 var attributeHook = require('virtual-dom/virtual-hyperscript/hooks/attribute-hook');
@@ -11,53 +13,59 @@ var attributeHook = require('virtual-dom/virtual-hyperscript/hooks/attribute-hoo
 var SVG_NAMESPACE = 'http://www.w3.org/2000/svg';
 
 function svg(tagName, properties, children) {
-    if (!children && isChildren(properties)) {
-        children = properties;
-        properties = {};
+  if (!children && isChildren(properties)) {
+    children = properties;
+    properties = {};
+  }
+
+  properties = properties || {};
+
+  // set namespace for svg
+  properties.namespace = SVG_NAMESPACE;
+
+  var attributes = properties.attributes || (properties.attributes = {});
+
+  for (var key in properties) {
+    if (!properties.hasOwnProperty(key)) {
+      continue;
     }
 
-    properties = properties || {};
+    var namespace = SVGAttributeNamespace(key);
 
-    // set namespace for svg
-    properties.namespace = SVG_NAMESPACE;
-
-    var attributes = properties.attributes || (properties.attributes = {});
-
-    for (var key in properties) {
-        if (!properties.hasOwnProperty(key)) {
-            continue;
-        }
-
-        var namespace = SVGAttributeNamespace(key);
-
-        if (namespace === undefined) { // not a svg attribute
-            continue;
-        }
-
-        var value = properties[key];
-
-        if (typeof value !== 'string' &&
-            typeof value !== 'number' &&
-            typeof value !== 'boolean'
-        ) {
-            continue;
-        }
-
-        if (namespace !== null) { // namespaced attribute
-            properties[key] = attributeHook(namespace, value);
-            continue;
-        }
-
-        attributes[key] = value
-        properties[key] = undefined
+    if (namespace === undefined) { // not a svg attribute
+      continue;
     }
 
-    return h(tagName, properties, children);
+    var value = properties[key];
+
+    if (typeof value !== 'string' &&
+      typeof value !== 'number' &&
+      typeof value !== 'boolean'
+    ) {
+      continue;
+    }
+
+    if (namespace !== null) { // namespaced attribute
+      properties[key] = attributeHook(namespace, value);
+      continue;
+    }
+
+    attributes[key] = value
+    properties[key] = undefined
+  }
+
+  return h(tagName, properties, children);
+}
+
+// START Cycle.js-specific code >>>>>>>>
+function isObservable(x) {
+  return x && typeof x.subscribe === 'function';
 }
 
 function isChildren(x) {
-    return typeof x === 'string' || isArray(x) || (x && typeof x.subscribe === 'function');
+  return typeof x === 'string' || isArray(x) || isObservable(x);
 }
+// END Cycle.js-specific code <<<<<<<<<<
 
 module.exports = svg;
 
