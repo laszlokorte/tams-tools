9edb18a2346252a10b4126ac197a01630e00e6c7
Laszlo Korte <me@laszlokorte.de>
2015-12-13 23:49:04 +0100

    detect pinch events

--- app/components/graphics/intent.js
+++ app/components/graphics/intent.js
@@ -2,68 +2,121 @@ import {Observable as O} from 'rx';
 
 import {preventDefault} from '../../lib/utils';
 
-const svgEventPosition = (evt, svg_) => {
-  const svg = svg_ || evt.target.ownerSVGElement || evt.target;
-  const pt = svg.createSVGPoint();
-  pt.x = evt.pageX;
-  pt.y = evt.pageY;
-  const result = pt.matrixTransform(svg.getScreenCTM().inverse());
-  return result;
+const svgEventPosition = (() => {
+  let oldPoint = null;
+  return ({x,y}, evt) => {
+    const svg = evt.target.ownerSVGElement || evt.target;
+    const pt = oldPoint || (oldPoint = svg.createSVGPoint());
+    pt.x = x;
+    pt.y = y;
+    const result = pt.matrixTransform(svg.getScreenCTM().inverse());
+    return result;
+  };
+})();
+
+const hammerPanOptions = (manager, Hammer) => {
+  manager.add(new Hammer.Pan({
+    direction: Hammer.DIRECTION_ALL,
+  }));
 };
 
-const hammerOptions = (manager, Hammer) => {
-  // Default pan recognizer.
-  manager.add(new Hammer.Pan());
-  // Default tap recognizer.
-  manager.get(`pan`).set({direction: Hammer.DIRECTION_ALL});
+const hammerPinchOptions = (manager, Hammer) => {
+  manager.add(new Hammer.Pinch());
 };
 
 export default (DOM) => {
-  const mousedown$ = DOM
+  const panStart$ = DOM
     .select('.graphics-root')
-    .events('panstart', hammerOptions)
-    .map((evt) => ({
-      start: svgEventPosition(evt.srcEvent),
-      svg: evt.target.ownerSVGElement || evt.target,
-    }));
-  const mouseUp$ = DOM
+    .events('panstart', hammerPanOptions);
+  const panEnd$ = DOM
     .select('.graphics-root')
     .events('panend');
-  const mouseMove$ = DOM
+  const panMove$ = DOM
     .select('.graphics-root')
     .events('panmove');
 
-  const drag$ = mousedown$
-    .flatMap(({start, svg}) =>
-      mouseMove$
-      .map((evt) => svgEventPosition(evt.srcEvent, svg))
-      .distinctUntilChanged(
-        (v) => v,
-        (a, b) => a.x === b.x && a.y === b.y
-      )
+  const pinchStart$ = DOM
+    .select('.graphics-root')
+    .events('pinchstart', hammerPinchOptions);
+  const pinchMove$ = DOM
+    .select('.graphics-root')
+    .events('pinchmove');
+  const pinchEnd$ = DOM
+    .select('.graphics-root')
+    .events('pinchend');
+
+  const pan$ = O.merge(
+    panStart$
+    .map((evt) => svgEventPosition(evt.center, evt.srcEvent))
+    .flatMap((start) =>
+      panMove$
+      .map((evt) => svgEventPosition(evt.center, evt.srcEvent))
       .map((target) => ({
         x: target.x - start.x,
         y: target.y - start.y,
-      })
-      ).takeUntil(mouseUp$)
-    );
+      }))
+      .takeUntil(panEnd$)
+    ),
+
+    pinchStart$
+      .map((evt) => svgEventPosition(evt.center, evt.srcEvent))
+      .flatMap((start) =>
+        pinchMove$
+        .map((evt) => svgEventPosition(evt.center, evt.srcEvent))
+        .map((target) => ({
+          x: target.x - start.x,
+          y: target.y - start.y,
+        }))
+        .takeUntil(pinchEnd$)
+      )
+  );
 
   return {
     zoom$:
-      DOM
-        .select('.graphics-root')
-        .events('wheel')
-        .do(preventDefault)
-        .map((evt) => {
-          const pivot = svgEventPosition(evt);
-          const wheel = evt.deltaY / -40;
-          const factor = Math.pow(1 + Math.abs(wheel) / 2 , wheel > 0 ? 1 : -1);
+      O.merge(
+        DOM
+          .select('.graphics-root')
+          .events('wheel')
+          .do(preventDefault)
+          .map((evt) => {
+            const pivot = svgEventPosition({
+              x: evt.clientX,
+              y: evt.clientY},
+            evt);
+            const wheel = evt.deltaY / -40;
+            const factor = Math.pow(
+              1 + Math.abs(wheel) / 2,
+              wheel > 0 ? 1 : -1
+            );
 
-          return {
-            factor,
-            pivot,
-          };
-        }),
-    pan$: drag$,
+            return {
+              factor,
+              pivot,
+            };
+          }),
+        pinchStart$
+          .flatMap((startEvt) =>
+            pinchMove$
+            .map((moveEvt) =>
+            ({
+              factor: moveEvt.scale,
+              pivot: svgEventPosition(moveEvt.center, moveEvt.srcEvent),
+            }))
+            .startWith({
+              factor: startEvt.scale,
+              prevFactor: startEvt.scale,
+              pivot: svgEventPosition(startEvt.center, startEvt.srcEvent)
+            })
+            .scan(
+              ({prevFactor}, {factor, pivot}) => ({
+                factor: factor / prevFactor,
+                prevFactor: factor,
+                pivot,
+              })
+            )
+            .takeUntil(pinchEnd$)
+          )
+      ),
+    pan$: pan$,
   };
 };
