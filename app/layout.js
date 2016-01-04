b2427214b0206111098a01fa70a18e3d7d55b3a7
Laszlo Korte <me@laszlokorte.de>
2016-01-04 04:55:35 +0100

    add slide animation

--- app/layout.js
+++ app/layout.js
@@ -3,3 +3,10 @@ import './styles/layout.styl';
 import html from 'raw!./layout.html';
 
 document.getElementById('app').innerHTML = html;
+
+const canvas = document.getElementById('canvas');
+document.getElementById('canvas-toggle').addEventListener('mousedown', (evt) => {
+  evt.preventDefault();
+
+  canvas.classList.toggle('state-closed');
+});
