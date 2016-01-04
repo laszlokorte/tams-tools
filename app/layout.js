d8a013fc17b2cf73f6decdc3b12562aec5fce3b6
Laszlo Korte <me@laszlokorte.de>
2016-01-04 06:09:26 +0100

    more animations

--- app/layout.js
+++ app/layout.js
@@ -10,3 +10,30 @@ document.getElementById('canvas-toggle').addEventListener('mousedown', (evt) => 
 
   canvas.classList.toggle('state-closed');
 });
+
+const editModeButtons = document.querySelectorAll('.edit-mode');
+const editButtons = document.querySelectorAll('.thumbnail-action, #add-output');
+const modeBar = document.getElementById('mode-bar');
+document.getElementById('edit-function').addEventListener('mousedown', (evt) => {
+  evt.preventDefault();
+
+  modeBar.classList.toggle('state-collapsed');
+  Array.prototype.forEach.call(editButtons, (e) => {
+    e.classList.toggle('state-hidden');
+  });
+  Array.prototype.forEach.call(editModeButtons, (e) => {
+    e.classList.toggle('state-collapsed');
+  });
+});
+
+document.getElementById('edit-loops').addEventListener('mousedown', (evt) => {
+  evt.preventDefault();
+
+  modeBar.classList.toggle('state-collapsed');
+  Array.prototype.forEach.call(editButtons, (e) => {
+    e.classList.toggle('state-hidden');
+  });
+  Array.prototype.forEach.call(editModeButtons, (e) => {
+    e.classList.toggle('state-collapsed');
+  });
+});
