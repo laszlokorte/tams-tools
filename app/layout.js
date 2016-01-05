938d4d67c533a241848b5fe7986f3e1cf0425763
Laszlo Korte <me@laszlokorte.de>
2016-01-05 04:18:15 +0100

    new layout

--- app/layout.js
+++ app/layout.js
@@ -4,38 +4,3 @@ import html from 'raw!./layout.html';
 
 document.getElementById('app').innerHTML = html;
 
-const canvas = document.getElementById('canvas');
-document.getElementById('canvas-toggle')
-  .addEventListener('mousedown', (evt) => {
-    evt.preventDefault();
-
-    canvas.classList.toggle('state-closed');
-  });
-
-const editModeButtons = document.querySelectorAll('.edit-mode');
-const editButtons =
-  document.querySelectorAll('.thumbnail-action, #add-output, .overlay-item');
-const modeBar = document.getElementById('mode-bar');
-document.getElementById('edit-function').addEventListener('click', (evt) => {
-  evt.preventDefault();
-
-  modeBar.classList.toggle('state-collapsed');
-  Array.prototype.forEach.call(editButtons, (e) => {
-    e.classList.toggle('state-hidden');
-  });
-  Array.prototype.forEach.call(editModeButtons, (e) => {
-    e.classList.toggle('state-collapsed');
-  });
-});
-
-document.getElementById('edit-loops').addEventListener('click', (evt) => {
-  evt.preventDefault();
-
-  modeBar.classList.toggle('state-collapsed');
-  Array.prototype.forEach.call(editButtons, (e) => {
-    e.classList.toggle('state-hidden');
-  });
-  Array.prototype.forEach.call(editModeButtons, (e) => {
-    e.classList.toggle('state-collapsed');
-  });
-});
