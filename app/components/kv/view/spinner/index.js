19b9a2ca2086566866096a99efd91ba0ad507da4
Laszlo Korte <me@laszlokorte.de>
2016-01-18 21:41:20 +0100

    simplify spinner buttons

--- app/components/kv/view/spinner/index.js
+++ app/components/kv/view/spinner/index.js
@@ -9,16 +9,14 @@ export default ({attributes, label, readonly, value, min, max}) =>
     attributes: attributes,
   }, [
     span('.spinner-label', label),
+    button('.spinner-button-decrement', {
+      attributes: {'data-spinner-action': 'decrement'},
+      disabled: min >= value,
+    }, 'Decrement'),
     span('.spinner-value', value.toString()),
-    span('.spinner-buttons', [
-      button('.spinner-button-decrement', {
-        attributes: {'data-spinner-action': 'decrement'},
-        disabled: min >= value,
-      }, 'Decrement'),
-      button('.spinner-button-increment', {
-        attributes: {'data-spinner-action': 'increment'},
-        disabled: max <= value,
-      }, 'Increment'),
-    ]),
+    button('.spinner-button-increment', {
+      attributes: {'data-spinner-action': 'increment'},
+      disabled: max <= value,
+    }, 'Increment'),
   ])
 ;
