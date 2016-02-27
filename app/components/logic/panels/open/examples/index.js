a4e63f6a72a3753a5b836c476e2f706e4d8e6186
Laszlo Korte <me@laszlokorte.de>
2016-02-27 17:16:59 +0100

    add more examples

--- app/components/logic/panels/open/examples/index.js
+++ app/components/logic/panels/open/examples/index.js
@@ -1,6 +1,38 @@
 export default [
   {
     name: "De Morgan",
-    data: require('./demorgan.json'),
+    data: {
+      langId: "math",
+      term: "(P ∧ Q), (¬((¬P) ∨ (¬Q)))",
+    },
   },
+  {
+    name: "Invalid Cycle",
+    data: {
+      langId: "c",
+      term: "A = P & Q, P = R | S, R = A",
+    },
+  },
+  {
+    name: "Dependencies",
+    data: {
+      langId: "c",
+      term: "A = P & Q, B = P | Q, C = A ^ B",
+    },
+  },
+  {
+    name: "Vector",
+    data: {
+      langId: "c",
+      term: "Y1 = <X1, X2, X3: 10011101>",
+    },
+  },
+  {
+    name: "Vector (Dont-Care)",
+    data: {
+      langId: "c",
+      term: "Y1 = <X1, X2, X3: 1**1010*>",
+    },
+  },
+
 ];
