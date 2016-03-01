99ab9891e98e367459882acb8c8da995db0dc249
Laszlo Korte <me@laszlokorte.de>
2016-03-01 21:43:43 +0100

    decompose into render function

--- app/pages/number-circle/index.js
+++ app/pages/number-circle/index.js
@@ -7,10 +7,29 @@ import {preventDefaultDriver} from '../../drivers/prevent-default';
 import {keyboardDriver} from '../../drivers/keyboard';
 import {globalEventDriver} from '../../drivers/global-events';
 
+// convert array of angles into svg
+const render = (angles) =>
+  div([
+    svg('svg', {
+      attributes: {
+        width: 200,
+        height: 200,
+        class: 'graphics-root',
+        viewBox: `0 0 500 500`,
+        preserveAspectRatio: 'xMidYMid meet',
+      },
+    }, angles.map((angle) =>
+      svg('circle', {
+        cx: 250 + Math.sin(angle) * 200,
+        cy: 250 + Math.cos(angle) * 200,
+        r: 50,
+      }))
+    ),
+  ])
+;
+
 const numberCircleApp = (sources) => {
-  return {
-    DOM:
-    // number of bits to generate the number circle for
+  const state$ = // number of bits to generate the number circle for
     O.just(3)
     // convert number of bit's into array of angles
     .map((bits) =>
@@ -19,27 +38,10 @@ const numberCircleApp = (sources) => {
         .apply(Array, {length: Math.pow(2, bits)})
         // map array to angles
         .map((_, index, all) => 2 * Math.PI * index / all.length)
-    )
-    // convert array of angles into svg
-    .map((angles) =>
-      div([
-        svg('svg', {
-          attributes: {
-            width: 200,
-            height: 200,
-            class: 'graphics-root',
-            viewBox: `0 0 500 500`,
-            preserveAspectRatio: 'xMidYMid meet',
-          },
-        }, angles.map((angle) =>
-          svg('circle', {
-            cx: 250 + Math.sin(angle) * 200,
-            cy: 250 + Math.cos(angle) * 200,
-            r: 50,
-          }))
-        ),
-      ])
-    ),
+    );
+
+  return {
+    DOM: state$.map(render),
   };
 };
 
