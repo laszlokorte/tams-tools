856adf4bf3703e6cec435073ee7d0b9bf747e85c
Laszlo Korte <me@laszlokorte.de>
2016-03-12 08:45:42 +0100

    simplify graphics model

--- app/components/graphics/model/index.js
+++ app/components/graphics/model/index.js
@@ -3,49 +3,38 @@ import {Observable as O} from 'rx';
 import {clamp} from '../../../lib/utils';
 import {ContentThunk} from '../../../lib/contentThunk';
 
-const clampPosition = (modifierFn, {minX, minY, maxX, maxY}) =>
-  (cam) => {
-    const {x,y,zoom} = modifierFn(cam);
-    return {
-      zoom,
-      x: clamp(x, minX, maxX),
-      y: clamp(y, minY, maxY),
-    };
-  }
-;
-
-const zoomModifier = (min, max) => ({factor, pivot}) => ({x, y, zoom}) => {
-  const newZoom = clamp(zoom * factor, min, max);
+const zoomModifier = ({factor, pivot}, zoomLimit, bounds) => ({x, y, zoom}) => {
+  const newZoom = clamp(zoom * factor, zoomLimit.min, zoomLimit.max);
   const realFactor = newZoom / zoom;
   const panFactor = (1 - 1 / realFactor);
 
   return {
     zoom: newZoom,
-    x: x + (pivot.x - x) * panFactor,
-    y: y + (pivot.y - y) * panFactor,
+    x: clamp(x + (pivot.x - x) * panFactor, bounds.minX, bounds.maxX),
+    y: clamp(y + (pivot.y - y) * panFactor, bounds.minY, bounds.maxY),
   };
 };
 
-const panModifier = (delta) => ({x, y, zoom}) => ({
+const panModifier = (delta, bounds) => ({x, y, zoom}) => ({
   zoom,
-  x: x - delta.x,
-  y: y - delta.y,
+  x: clamp(x - delta.x, bounds.minX, bounds.maxX),
+  y: clamp(y - delta.y, bounds.minY, bounds.maxY),
 });
 
-const autoCenterModifier = (minZoom, {
+const autoCenterModifier = (_, zoomLimit, bounds, {
   width, height, pivotX = 0.5, pivotY = 0,
-}) => ({minX, minY, maxX, maxY}) => {
-  const centerX = (maxX + minX) / 2;
-  const centerY = (maxY + minY) / 2;
-  const contentWidth = maxX - minX;
-  const contentHeight = maxY - minY;
+}) => {
+  const centerX = (bounds.maxX + bounds.minX) / 2;
+  const centerY = (bounds.maxY + bounds.minY) / 2;
+  const contentWidth = bounds.maxX - bounds.minX;
+  const contentHeight = bounds.maxY - bounds.minY;
   const xRatio = (width - 20) / contentWidth;
   const zoom = xRatio;
 
   return () => ({
     x: centerX + (pivotX - 0.5) * contentWidth,
     y: centerY + (pivotY - 0.5) * contentHeight + height / 2 / zoom,
-    zoom: clamp(zoom, minZoom, 5),
+    zoom: clamp(zoom, zoomLimit.min, zoomLimit.max),
   });
 };
 
@@ -64,25 +53,23 @@ export default ({
   return O.combineLatest(
     props$,
     camera$,
-    (props, initCamera) =>
-      forceBounds$.flatMapLatest((bounds) => {
-        const minZoom = Math.min(
+    (props, initCamera) => {
+      const zoomLimit$ = bounds$.map((bounds) => ({
+        min: Math.min(
           props.width / (bounds.maxX - bounds.minX),
           props.height / (bounds.maxY - bounds.minY),
           0.5
-        );
+        ),
+        max: 5,
+      }));
 
-        return O.merge([
-          actions.zoom$.map(zoomModifier(minZoom, 5)),
-          actions.pan$.map(panModifier),
-          forceBounds$.map(autoCenterModifier(minZoom, props)),
-        ]).map((mod) => clampPosition(mod, bounds))
-        .startWith(({x, y, zoom}) => ({
-          zoom,
-          x: clamp(x, bounds.minX, bounds.maxX),
-          y: clamp(y, bounds.minY, bounds.maxY),
-        }));
-      })
+      return O.merge([
+        actions.zoom$.withLatestFrom(zoomLimit$, bounds$, zoomModifier),
+        actions.pan$.withLatestFrom(bounds$, panModifier),
+        forceBounds$.withLatestFrom(
+          zoomLimit$, bounds$, props$, autoCenterModifier
+        ),
+      ])
       .startWith({
         zoom: initCamera.zoom,
         x: initCamera.x,
@@ -95,6 +82,6 @@ export default ({
         camera: cam,
         bounds,
         content,
-      }))
-  ).switch();
+      }));
+    }).switch();
 };
