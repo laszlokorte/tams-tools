a02b70cf4cf3b31a0549eed60922250ee216f49d
Laszlo Korte <me@laszlokorte.de>
2016-03-15 11:51:37 +0100

    fix textarea resize

--- app/drivers/textarea-resize.js
+++ app/drivers/textarea-resize.js
@@ -10,8 +10,20 @@ export const autoResizeDriver = (event$) => {
     }
     animating = requestAnimationFrame(() => {
       animating = null;
+
+      /*
+        usually just this would work:
+        element.style.height = element.scrollHeight + 'px';
+
+        but in firefox 18 scrollHeight does not include padding
+      */
+
       element.style.height = '0px';
-      element.style.height = element.scrollHeight + 'px';
+      const paddingVertical = element.offsetHeight;
+      element.style.padding = '0px';
+      const newHeight = element.scrollHeight + paddingVertical;
+      element.style.padding = null;
+      element.style.height = newHeight + 'px';
     });
   });
 
