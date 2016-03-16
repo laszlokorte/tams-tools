laszlokorte/tams-tools

commit a52b7a0ac43d890d72f013bf7c38f440bec232ae

reduce line length

app/components/graphics/model/index.js
@@ -32,8 +32,14 @@ const autoCenterModifier = (_, zoomLimit, bounds, {
   const zoom = xRatio;
 
   return () => ({
-    x: clamp(centerX + (pivotX - 0.5) * contentWidth, bounds.minX, bounds.maxX),
-    y: clamp(centerY + (pivotY - 0.5) * contentHeight + height / 2 / zoom, bounds.minY, bounds.maxY),
+    x: clamp(
+      centerX + (pivotX - 0.5) * contentWidth,
+      bounds.minX, bounds.maxX
+    ),
+    y: clamp(
+      centerY + (pivotY - 0.5) * contentHeight + height * zoom / 2,
+      bounds.minY, bounds.maxY
+    ),
     zoom: clamp(zoom, zoomLimit.min, zoomLimit.max),
   });
 };

commit 234963c4a4fbfd2a2e3c17341b558e801bf86bd9

revert graphics auto center regression
it was introduced by a52b7a0ac43d

app/components/graphics/model/index.js
@@ -37,7 +37,8 @@ const autoCenterModifier = (_, zoomLimit, bounds, {
       bounds.minX, bounds.maxX
     ),
     y: clamp(
-      centerY + (pivotY - 0.5) * contentHeight + height * zoom / 2,
+      height / zoom / 2 +
+      centerY + (pivotY - 0.5) * contentHeight,
       bounds.minY, bounds.maxY
     ),
     zoom: clamp(zoom, zoomLimit.min, zoomLimit.max),