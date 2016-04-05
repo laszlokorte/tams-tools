77a1216016d07adf550a4585759bcd31b6a44d42
Laszlo Korte <me@laszlokorte.de>
2016-04-05 23:46:09 +0200

    improve split pane touch handling
    for position calculation ignore touches that are outside the handle

--- app/components/splitpane/intent/index.js
+++ app/components/splitpane/intent/index.js
@@ -7,14 +7,20 @@ import {Observable as O} from 'rx';
 // handling here.
 
 // the center of all touches of the given touch event
-const touchCenter = (touchEvt) => ({
-  x: Array.prototype.reduce.call(touchEvt.touches,
-    (acc, t) => acc + t.pageX, 0
-  ) / touchEvt.touches.length,
-  y: Array.prototype.reduce.call(touchEvt.touches,
-    (acc, t) => acc + t.pageY, 0
-  ) / touchEvt.touches.length,
-});
+const touchCenter = (target, touchEvt) => {
+  const touches = Array.prototype.filter.call(
+    touchEvt.touches, (t) => t.target === target
+  );
+
+  return {
+    x: Array.prototype.reduce.call(touches,
+      (acc, t) => acc + t.pageX, 0
+    ) / (touches.length || 1),
+    y: Array.prototype.reduce.call(touches,
+      (acc, t) => acc + t.pageY, 0
+    ) / (touches.length || 1),
+  };
+};
 
 export default (DOM, globalEvents) => {
   const handle = DOM.select('.splitpane-handle');
@@ -34,14 +40,17 @@ export default (DOM, globalEvents) => {
       y: evt.pageY,
       event: evt,
     })),
-    globalEvents.events('touchmove').map((evt) => {
-      const center = touchCenter(evt);
-      return {
-        x: center.x,
-        y: center.y,
-        event: evt,
-      };
-    })
+    globalEvents.events('touchmove').withLatestFrom(
+      handle.observable,
+      (evt, target) => {
+        const center = touchCenter(target[0], evt);
+        return {
+          x: center.x,
+          y: center.y,
+          event: evt,
+        };
+      }
+    )
   );
 
   // use either mouseup or touchend/touchcancel events
