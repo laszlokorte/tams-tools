909215f7b18e1a903f73dd9389f4fef096fea451
Laszlo Korte <me@laszlokorte.de>
2016-04-05 21:03:47 +0200

    trigger input event when inserting text

--- app/drivers/rangy.js
+++ app/drivers/rangy.js
@@ -1,10 +1,22 @@
 import {replaceSelectedText} from '../lib/rangy';
 
+const triggerInputEvent = (element) => {
+  if ("createEvent" in document) {
+    const evt = document.createEvent("MutationEvents");
+    evt.initEvent("input", true, true);
+    element.dispatchEvent(evt);
+  } else {
+    element.fireEvent("oninput");
+  }
+};
+
 // This driver consumes an Observable of objects
 // {element, string} and insertes string into the element
 export const insertStringDriver = (insertion$) => {
   insertion$.subscribe((insertion) => {
-    replaceSelectedText(insertion.element, insertion.string, "collapseToEnd");
+    const element = insertion.element;
+    replaceSelectedText(element, insertion.string, "collapseToEnd");
+    triggerInputEvent(element);
   });
 
   return Object.create(null);
